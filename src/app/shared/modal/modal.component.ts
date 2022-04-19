fbcde7813fa925ee6de3ef666531b58c2ba1056f
Adham-Niazy <adhamniazy@gmail.com>
2022-04-19 22:40:25 +0200

    Fix CSS issue regarding reusable modal component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,9 +9,13 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID: string = "";
 
-  constructor(public modal: ModalService) { }
+  constructor(
+    public modal: ModalService,
+    private el: ElementRef
+  ) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal() {
