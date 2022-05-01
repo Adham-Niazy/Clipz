0137fe1f5ea08a40b129658d0fdbab5ccf12873c
Adham-Niazy <adhamniazy@gmail.com>
2022-05-01 05:07:08 +0200

    Destroying the auth modal after Success

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalID: string = "";
 
   constructor(
@@ -20,5 +20,9 @@ export class ModalComponent implements OnInit {
 
   closeModal() {
     this.modal.toggleModalVisibility(this.modalID);
+  }
+
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement);
   }
 }
