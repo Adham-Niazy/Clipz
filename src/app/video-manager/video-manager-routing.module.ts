d85812637e419479d2960d674cf5c67c9a0c7ac3
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 01:14:16 +0200

    Adding Upload component + Managing its routing config

--- src/app/video-manager/video-manager-routing.module.ts
+++ src/app/video-manager/video-manager-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
+    data: {
+      authOnly: true
+    }
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
     data: {
       authOnly: true
     }
--- src/app/video-manager/video-manager.module.ts
+++ src/app/video-manager/video-manager.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { VideoManagerRoutingModule } from './video-manager-routing.module';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
 
 @NgModule({
   declarations: [
-    ManageComponent
+    ManageComponent,
+    UploadComponent
   ],
   imports: [
     CommonModule,
