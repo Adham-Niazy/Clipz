22e3524b58b0888994c08177db381cd50df40d87
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 02:55:13 +0200

    Adding Auth Guards to both (Manage/Upload) Components

--- src/app/video-manager/video-manager-routing.module.ts
+++ src/app/video-manager/video-manager-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthGuard]
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthGuard]
   }
 ];
 
