276db34a01dd9082ddb10c82db713309f252210f
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 17:09:42 +0200

    Adding timestamp

--- src/app/video-manager/upload/upload.component.ts
+++ src/app/video-manager/upload/upload.component.ts
@@ -95,7 +95,8 @@ export class UploadComponent implements OnDestroy {
           displayName: this.user?.displayName as string,
           title: this.title.value,
           fileName: `${clipFileName}.mp4`,
-          url
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
         const clipDocRef = await this.clipsService.createClip(clip);
         this.updateAlert({
