3d75c838c42f944a9cbd37b05f70b7df93b3318c
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 18:06:22 +0200

    Listing clips of specific user through service + displaying them

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { switchMap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
 
 @Injectable({
@@ -9,12 +12,30 @@ export class ClipService {
   private clipsCollection: AngularFirestoreCollection<IClip>;
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) {
     this.clipsCollection = this.db.collection('clips');
   }
 
   public createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data)
+  }
+
+  public getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user => {
+        if(!user) {
+          return of([])
+        }
+
+        const query = this.clipsCollection.ref.where(
+          'uid', '==', user.uid
+        );
+
+        return query.get();
+      }),
+      map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
+    )
   }
 }
--- src/app/video-manager/manage/manage.component.ts
+++ src/app/video-manager/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -8,21 +10,32 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   sort_order: string = '1';
+  clips: IClip[] = [];
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private clipService: ClipService
   ) { }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.sort_order = params['get']('sort') ?? '1';
-    })
+    });
+
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = [];
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
+    });
   }
 
   sort(e: Event) {
     const { value } = (e.target as HTMLSelectElement);
-
     this.router.navigateByUrl(`/manage?sort=${value}`)
   }
 }
