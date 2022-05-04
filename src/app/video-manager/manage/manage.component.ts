1bfb87c794330ff8c2ee57350e2551e0f61b43b8
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 02:12:54 +0200

    Adjusting sort query params inside manage component routing

--- src/app/video-manager/manage/manage.component.ts
+++ src/app/video-manager/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.scss']
 })
 export class ManageComponent implements OnInit {
+  sort_order: string = '1';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.sort_order = params['get']('sort') ?? '1';
+    })
   }
 
+  sort(e: Event) {
+    const { value } = (e.target as HTMLSelectElement);
+
+    this.router.navigateByUrl(`/manage?sort=${value}`)
+  }
 }
