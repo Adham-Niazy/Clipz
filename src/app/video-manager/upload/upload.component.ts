9a5601b5e39cac5252406d0c3f1cd50c431965dc
Adham-Niazy <adhamniazy@gmail.com>
2022-05-04 04:09:16 +0200

    Handle File Extraction

--- src/app/video-manager/upload/upload.component.ts
+++ src/app/video-manager/upload/upload.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent implements OnInit {
+  isDragover: boolean = false;
+  file: File | null = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  storeFile(e: Event) {
+    this.isDragover = false;
+
+    this.file = (e as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
   }
 
 }
