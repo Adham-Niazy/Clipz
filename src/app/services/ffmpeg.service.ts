cd52f37139fe2922da22af3e0ce6cca565eb5d67
Adham-Niazy <adhamniazy@gmail.com>
2022-05-19 05:49:30 +0200

    Generating a single ScreenShot

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -23,7 +23,21 @@ export class FfmpegService {
 
   async getScreenshots(file: File) {
     const data = await fetchFile(file);
-
+    // Store the uploaded memory in the Buffered Array
     this.ffmpeg.FS('writeFile', file.name, data);
+    // Run commands through ffmpeg API to manipulate the video
+    await this.ffmpeg.run(
+      // Input: -i is to grap a specific file in file system
+      '-i', file.name,
+      // Output options
+      // 1) Update timestamp to tell ffmpeg to capture a specific screenshot time 
+      '-ss', '00:00:01',
+      // 2) since we need an image so here I am defining the frames rate to 1
+      '-frames:v', '1',
+      // 3) then resize the screen shot to be always 510 width and height depends on aspect ratio
+      '-filter:v', 'scale=510:-1',
+      // Output
+      'output_01.png'
+    )
   }
 }
