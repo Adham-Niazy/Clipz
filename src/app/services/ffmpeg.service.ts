248ab353f1cd8aa217c673d405a31c8a081c3967
Adham-Niazy <adhamniazy@gmail.com>
2022-05-19 07:17:37 +0200

    Generating Multiple Screenshots

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -26,18 +26,28 @@ export class FfmpegService {
     // Store the uploaded memory in the Buffered Array
     this.ffmpeg.FS('writeFile', file.name, data);
     // Run commands through ffmpeg API to manipulate the video
+
+    const seconds = [1, 2, 3];
+    const commands: string[] = [];
+
+    seconds.forEach(sec => {
+      commands.push(
+        // Input: -i is to grap a specific file in file system
+        '-i', file.name,
+        // Output options
+        // 1) Update timestamp to tell ffmpeg to capture a specific screenshot time 
+        '-ss', `00:00:0${sec}`,
+        // 2) since we need an image so here I am defining the frames rate to 1
+        '-frames:v', '1',
+        // 3) then resize the screen shot to be always 510 width and height depends on aspect ratio
+        '-filter:v', 'scale=510:-1',
+        // Output
+        `output_0${sec}.png`
+      )
+    });
+
     await this.ffmpeg.run(
-      // Input: -i is to grap a specific file in file system
-      '-i', file.name,
-      // Output options
-      // 1) Update timestamp to tell ffmpeg to capture a specific screenshot time 
-      '-ss', '00:00:01',
-      // 2) since we need an image so here I am defining the frames rate to 1
-      '-frames:v', '1',
-      // 3) then resize the screen shot to be always 510 width and height depends on aspect ratio
-      '-filter:v', 'scale=510:-1',
-      // Output
-      'output_01.png'
+      ...commands
     )
   }
 }
