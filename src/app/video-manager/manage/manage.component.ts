91e406927ea7ae4f02ec375d13418b351b902e86
Adham-Niazy <adhamniazy@gmail.com>
2022-05-21 17:41:59 +0200

    Copy links to the clipboard

--- src/app/video-manager/manage/manage.component.ts
+++ src/app/video-manager/manage/manage.component.ts
@@ -67,4 +67,14 @@ export class ManageComponent implements OnInit {
     this.modal.toggleModalVisibility('edit-clip');
     this.active_clip = clip_info;
   }
+
+  async copyToClipboard(event: MouseEvent, docID: string | undefined) {
+    event.preventDefault();
+    if (!docID) return;
+
+    const url = `${location.origin}/clip/${docID}`;
+
+    await navigator.clipboard.writeText(url);
+    alert('Link Copied!')
+  }
 }
